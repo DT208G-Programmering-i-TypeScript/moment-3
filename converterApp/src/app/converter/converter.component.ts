11c5d04779dbf3548945956acebe586577daae90
Babak Jameei <babak.jameei@outlook.com>
2024-05-02 18:42:18 +0200

    Funktionalitet för konvertering tillagd, vice-versa måste lösas

--- converterApp/src/app/converter/converter.component.ts
+++ converterApp/src/app/converter/converter.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-converter',
-  standalone: true,
-  imports: [],
   templateUrl: './converter.component.html',
-  styleUrl: './converter.component.css'
+  styleUrls: ['./converter.component.css'],
+  standalone: true,
+  imports: [FormsModule]
+  
 })
 export class ConverterComponent {
+  @Input() meterValue: number = 0;
+  @Input() feetValue: number = 0;
+  @Input() celsiusValue: number = 0;
+  @Input() fahrenheitValue: number = 0;
 
+  resetLength() {
+
+    this.meterValue = 0;
+    this.feetValue = 0;
+  }
+
+  resetTemperature() {
+    this.celsiusValue = 0;
+    this.fahrenheitValue = 0;
+  }
+
+  updateLength() {
+    if (this.meterValue) {
+      this.feetValue = this.meterValue * 3.28084;
+    } else {
+      this.meterValue = this.feetValue / 3.28084;
+    }
+  }
+
+  updateTemperature() {
+    if (this.celsiusValue) {
+      this.fahrenheitValue = (this.celsiusValue * 9/5) + 32;
+    } else {
+      this.celsiusValue = (this.fahrenheitValue - 32) * 5/9;
+    }
+  }
 }
