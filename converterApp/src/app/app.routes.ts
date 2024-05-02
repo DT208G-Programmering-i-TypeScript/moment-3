5037bae14e0be3b7bc96f988aeec41459120272e
Babak Jameei <babak.jameei@outlook.com>
2024-05-02 17:03:09 +0200

    routing to components created

--- converterApp/src/app/app.routes.ts
+++ converterApp/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ConverterComponent } from './converter/converter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'converter', component: ConverterComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
