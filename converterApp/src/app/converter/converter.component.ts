2d291f66ffb87073079ce1ef6b91945b1d56304a
Babak Jameei <babak.jameei@outlook.com>
2024-10-15 15:56:40 +0200

    Värden converteras korrektr åt båda hållen, styling tillagd som css komponent samt kommntarer tillagda

--- converterApp/src/app/converter/converter.component.ts
+++ converterApp/src/app/converter/converter.component.ts
@@ -1,43 +1,45 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-converter',
-  templateUrl: './converter.component.html',
-  styleUrls: ['./converter.component.css'],
-  standalone: true,
-  imports: [FormsModule]
-  
+  selector: 'app-converter', // Komponentens selektor, används för att inkludera komponenten i en HTML-fil
+  templateUrl: './converter.component.html', // HTML-mallen för komponenten
+  styleUrls: ['./converter.component.css'], // CSS-filstilar som används för komponenten
+  standalone: true, // Standalone-komponent som inte är beroende av en modul
+  imports: [FormsModule] // Importerar FormsModule för att hantera formulärbindningar
 })
 export class ConverterComponent {
-  @Input() meterValue: number = 0;
-  @Input() feetValue: number = 0;
-  @Input() celsiusValue: number = 0;
-  @Input() fahrenheitValue: number = 0;
+  @Input() meterValue: number | null = null;
+  @Input() feetValue: number | null = null;
+  @Input() celsiusValue: number | null = null;
+  @Input() fahrenheitValue: number | null = null;
 
+  // Nollställer längdenheter (meter och feet)
   resetLength() {
-
-    this.meterValue = 0;
-    this.feetValue = 0;
+    this.meterValue = null;
+    this.feetValue = null;
   }
 
+  // Nollställer temperaturenheter (celsius och fahrenheit)
   resetTemperature() {
-    this.celsiusValue = 0;
-    this.fahrenheitValue = 0;
+    this.celsiusValue = null;
+    this.fahrenheitValue = null;
   }
 
-  updateLength() {
-    if (this.meterValue) {
+  // Uppdaterar längdenheterna beroende på inmatningskälla (meter eller feet)
+  updateLength(source: 'meter' | 'feet') {
+    if (source === 'meter' && this.meterValue !== null) {
       this.feetValue = this.meterValue * 3.28084;
-    } else {
+    } else if (source === 'feet' && this.feetValue !== null) {
       this.meterValue = this.feetValue / 3.28084;
     }
   }
 
-  updateTemperature() {
-    if (this.celsiusValue) {
+   // Uppdaterar temperaturenheterna beroende på inmatningskälla (celsius eller fahrenheit)
+  updateTemperature(source: 'celsius' | 'fahrenheit') {
+    if (source === 'celsius' && this.celsiusValue !== null) {
       this.fahrenheitValue = (this.celsiusValue * 9/5) + 32;
-    } else {
+    } else if (source === 'fahrenheit' && this.fahrenheitValue !== null) {
       this.celsiusValue = (this.fahrenheitValue - 32) * 5/9;
     }
   }
